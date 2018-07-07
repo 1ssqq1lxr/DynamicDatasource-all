97ea312fd11a970298a60a0d4fe21b3013bfd290
张宇涛 <13770504450@163.com>
2018-07-07 12:27:20 +0800

    init commit

--- src/main/java/com/jswym/spring/boot/starter/multidatasource/properties/DataSourceProperties.java
+++ src/main/java/com/jswym/spring/boot/starter/multidatasource/properties/DataSourceProperties.java
@@ -3,8 +3,14 @@ package com.jswym.spring.boot.starter.multidatasource.properties;
 
 public class DataSourceProperties extends org.springframework.boot.autoconfigure.jdbc.DataSourceProperties {
 
+    /**
+     * 租户id
+     */
     private long tenantId;
 
+    /**
+     * 机构编码
+     */
     private String orgcode;
 
 }
