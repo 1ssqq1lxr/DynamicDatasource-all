9b91bf5e90c30cd3ca9f78877186f0a9e117c565
luxurong <luxurong@jswym.com>
2018-07-09 11:37:07 +0800

    多数据源修改

--- src/main/java/com/jswym/spring/boot/starter/multidatasource/properties/DataSourceProperties.java
+++ src/main/java/com/jswym/spring/boot/starter/multidatasource/properties/DataSourceProperties.java
@@ -1,16 +1,68 @@
 package com.jswym.spring.boot.starter.multidatasource.properties;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-public class DataSourceProperties extends org.springframework.boot.autoconfigure.jdbc.DataSourceProperties {
-
-    /**
-     * 租户id
-     */
-    private long tenantId;
+@Setter
+@Getter
+@ToString
+public class DataSourceProperties{
 
     /**
      * 机构编码
      */
     private String orgcode;
+    /**
+     * 是否生效
+     */
+    private boolean enabled;
+
+    /**
+     * 是否是默认数据库  配置多个 默认选择第一个
+     */
+    private boolean isDefault;
+
+    /**
+     * 连接超时
+     */
+    private  long connectionTimeout;
+    /**
+     * 校验超时
+     */
+    private  long validationTimeout;
+    /**
+     * 断开连接等
+     */
+    private  long idleTimeout;
+    /**
+     * 连接被占用的超时时间
+     */
+    private  long leakDetectionThreshold;
+    /**
+     * 最大存活时间
+     */
+    private  long maxLifetime;
+    /**
+     * 连接池最大连接数
+     */
+    private  int maxPoolSize;
+
+    /**
+     * 最小存活时间
+     */
+    private  int minIdle;
+    /**
+     * 用户名
+     */
+    private  String username;
+    /**
+     * 密码
+     */
+    private  String password;
+    /**
+     * jdbc url
+     */
+    private  String jdbcUrl;
 
 }
--- src/main/java/com/jswym/spring/boot/starter/multidatasource/properties/MultiDatasourceProperties.java
+++ src/main/java/com/jswym/spring/boot/starter/multidatasource/properties/MultiDatasourceProperties.java
@@ -1,42 +1,24 @@
 package com.jswym.spring.boot.starter.multidatasource.properties;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
 import java.util.Map;
 
 /**
  * 配置
  */
+@Getter
+@Setter
 @ConfigurationProperties(prefix = MultiDatasourceProperties.MULTI_DATASOURCE_PREFIX)
 public class MultiDatasourceProperties {
 
-    public static final String  MULTI_DATASOURCE_PREFIX= "wym.multidatasource";
+    public static final String  MULTI_DATASOURCE_PREFIX= "application.wym";
 
-    /**
-     * 是否生效
-     */
-    private boolean enabled;
 
-    /**
-     * 规则来源
-     */
-    private String gitSource;
-
-    /**
-     * 规则来源认证用户名
-     */
-    private String username;
-
-    /**
-     * 规则来源认证密码
-     */
-    private String password;
-
-    /**
-     * 多数据源
-     */
-    private Map<String, DataSourceProperties> datasourceMap;
-
+    private List<DataSourceProperties> multidatasource;
 
 
 }
