b5a64aaa06bf151866b267c0eb87b3589b74160f
张宇涛 <13770504450@163.com>
2018-07-25 16:48:55 +0800

    Revert "修改获取orgcode的方式"
    This reverts commit b7c4d76

--- src/main/java/com/jswym/spring/boot/starter/multidatasource/filter/WymFilter.java
+++ src/main/java/com/jswym/spring/boot/starter/multidatasource/filter/WymFilter.java
@@ -1,18 +1,13 @@
 package com.jswym.spring.boot.starter.multidatasource.filter;
 
 import com.jswym.spring.boot.starter.multidatasource.DynamicIdSelector;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.WebUtils;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -26,9 +21,6 @@ public class WymFilter extends OncePerRequestFilter {
 
 
     private final DynamicIdSelector dynamicIdSelector;
-
-    private final Logger log = LoggerFactory.getLogger(WymFilter.class);
-
 
     public WymFilter(DynamicIdSelector dynamicIdSelector) {
         this.dynamicIdSelector = dynamicIdSelector;
@@ -70,24 +62,6 @@ public class WymFilter extends OncePerRequestFilter {
     }
 
     private String obtainParameter(HttpServletRequest request) {
-
-        String orgcode = "";
-
-        String requestParameter = request.getParameter("orgcode");
-        log.info("requestParameter: {}", requestParameter);
-        Cookie cookie = WebUtils.getCookie(request, "orgcode");
-        log.info("cookie: {}", cookie.toString());
-        String header = request.getHeader("orgcode");
-        log.info("header: {}", header);
-        if (StringUtils.isNotBlank(requestParameter)) {
-            orgcode = requestParameter;
-        } else if (cookie != null && StringUtils.isNotBlank(cookie.toString())) {
-            orgcode = cookie.toString();
-        }else if (StringUtils.isNotBlank(header)) {
-            orgcode = header;
-        }else {
-            orgcode = "123";
-        }
-        return orgcode;
+       return  request.getParameter("orgcode");
     }
 }
